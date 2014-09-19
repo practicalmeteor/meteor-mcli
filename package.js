130e961110b2dac63609abaf6b358e4823f106f5
Juan Sepulveda <juan@lavaina.com>
2014-09-19 14:26:05 -0400

    Update to new meteor-package system

--- package.js
+++ package.js
@@ -3,16 +3,19 @@ Npm.depends({
 });
 
 Package.describe({
-  summary: "Package for creating cli programs with meteor"
+  summary: "Package for creating cli programs with meteor",
+  name: "spacejamio:cli",
+  version: "0.2.0",
+  git: "https://github.com/spacejamio/meteor-cli.git"
 });
 
 Package.on_use(function (api, where) {
-  api.use(["coffeescript","chai","sinon"], "server");
+  api.use(["coffeescript","spacejamio:chai@0.1.6"], "server");
 
   api.add_files("CLI.coffee", "server");
 });
 
 Package.on_test(function(api) {
-  api.use(['underscore', "coffeescript", "chai", "sinon", "munit", "meteor-cli"]);
+  api.use(['underscore', "coffeescript", "spacejamio:chai@0.1.6", "spacejamio:sinon@0.1.6", "spacejamio:munit@0.2.1", "meteor-cli"]);
   api.add_files("tests/CLITest.coffee", "server")
 });
