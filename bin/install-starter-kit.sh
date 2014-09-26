3385e06edb982ff840b74e869dd756e01eea66be
Ronen Babayoff <ronen@lavaina.com>
2014-09-26 03:34:36 -0400

    Fix install starter script

--- bin/install-starter-kit.sh
+++ bin/install-starter-kit.sh
@@ -4,5 +4,5 @@ sudo curl -L -o /tmp/meteor-cli.tar.gz https://github.com/spacejamio/meteor-cli/
 sudo tar xf /tmp/meteor-cli.tar.gz -C /tmp
 sudo mv /tmp/meteor-cli-master/bin/mcli /tmp/meteor-cli-master/bin/mcli-bundle /usr/local/bin
 sudo chmod 555 /usr/local/bin/mcli /usr/local/bin/mcli-bundle
-cp /tmp/meteor-cli-master/starter-cli-app .
-# sudo rm -rf /tmp/meteor-cli.tar.gz /tmp/meteor-cli-master
+cp -r /tmp/meteor-cli-master/starter-cli-app .
+sudo rm -rf /tmp/meteor-cli.tar.gz /tmp/meteor-cli-master
