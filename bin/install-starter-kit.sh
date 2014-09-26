0f276625b9ee5b8627e003efbebbbb48e19fc1f2
Ronen Babayoff <ronen@lavaina.com>
2014-09-26 03:25:54 -0400

    Add install-starter-kit.sh script

---
+++ bin/install-starter-kit.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -xe
+
+sudo curl -L -o /tmp/meteor-cli.tar.gz https://github.com/spacejamio/meteor-cli/archive/master.tar.gz
+sudo tar xf /tmp/meteor-cli.tar.gz -C /tmp
+sudo mv /tmp/meteor-cli-master/bin/mcli /tmp/meteor-cli-master/bin/mcli-bundle /usr/local/bin
+sudo chmod 555 /usr/local/bin/mcli /usr/local/bin/mcli-bundle
+cp /tmp/meteor-cli-master/starter-cli-app .
+sudo rm -rf /tmp/meteor-cli.tar.gz /tmp/meteor-cli-master
