e18d81d43f3e683e296baa5583bd92328729211d
Ronen Babayoff <ronen@lavaina.com>
2014-09-26 20:07:13 -0400

    fix and rename test-all script

--- bin/test-all.sh
+++ bin/test-all.sh
@@ -1,7 +1,9 @@
-#!/bin/bash -e
+#!/bin/bash -xe
 
 export PATH=$PWD/bin:$PATH
 spacejam test-packages ./
+export PACKAGE_DIRS=$(dirname $PWD)
+cd starter-cli-app
 mcli hello-world
 mcli echo --stderr=true I am an stderr echo
 mcli find-one --collection Todos
