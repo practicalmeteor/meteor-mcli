182eef4e11a108d711db7d10d4aff1b8303c4d61
Ronen Babayoff <ronen@lavaina.com>
2014-12-14 17:13:29 -0400

    Add console warning message for new package name

--- src/log.js
+++ src/log.js
@@ -1,1 +1,3 @@
 log = loglevel.createPackageLogger('practicalmeteor:mcli', 'info');
+
+console.warn('The spacjamio:mcli package has been renamed to practicalmeteor:mcli. Please use the new package name instead.');
