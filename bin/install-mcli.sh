397a42761e085faf742b477e81cf561fdfc6171b
James Lefrère <jlefrere@gmail.com>
2015-01-08 19:22:26 +0100

    Github URL fix

--- bin/install-mcli.sh
+++ bin/install-mcli.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-sudo curl -o /usr/local/bin/mcli https://raw.githubusercontent.com/practicalmeteor/meteor-cli/master/bin/mcli
+sudo curl -o /usr/local/bin/mcli https://raw.githubusercontent.com/practicalmeteor/meteor-mcli/master/bin/mcli
 sudo chmod 555 /usr/local/bin/mcli
-sudo curl -o /usr/local/bin/mcli-bundle https://raw.githubusercontent.com/practicalmeteor/meteor-cli/master/bin/mcli-bundle
+sudo curl -o /usr/local/bin/mcli-bundle https://raw.githubusercontent.com/practicalmeteor/meteor-mcli/master/bin/mcli-bundle
 sudo chmod 555 /usr/local/bin/mcli-bundle
